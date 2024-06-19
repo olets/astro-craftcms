1aebe7752b73583f9035bf28a2ba121cfb60c524
Henry Bley-Vroman <olets@olets.dev>
2024-06-18 14:12:26 -1000

    feat(app>home): redirect to custom route

--- packages/app/astro.config.mjs
+++ packages/app/astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: import.meta.env.DEV ? 'server' : 'static',
+  redirects: {
+    '/': '/example-route'
+  }
 });
