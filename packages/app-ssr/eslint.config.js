ee6856374d39f477e9884287aa696c46e2047e1a
Henry Bley-Vroman <olets@olets.dev>
2024-06-28 11:56:18 -1000

    feat(apps): configure eslint and prettier...
    First in packages/app-ssg, then in packages/app-ssr
    >>> bun add -D @eslint/compat eslint-config-prettier prettier

--- packages/app-ssr/eslint.config.js
+++ packages/app-ssr/eslint.config.js
@@ -1,12 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
 ];
