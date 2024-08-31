238023eda6104c805a81e305c03d3a56b57c48d9
Henry Bley-Vroman <olets@olets.dev>
2024-08-30 23:33:15 -0400

    style(app-ssg-cache-collection>content): alphabetize collections

--- packages/app-ssg-cache-collection/src/content/config.ts
+++ packages/app-ssg-cache-collection/src/content/config.ts
@@ -1,21 +1,21 @@
 import { defineCollection } from 'astro:content';
-import exampleRouteConfig from '@config/example-route';
 import exampleChannelConfig from '@config/example-channel';
+import exampleRouteConfig from '@config/example-route';
 import exampleSingleConfig from '@config/example-single';
 import exampleStructureConfig from '@config/example-structure';
 import indexConfig from '@config/index';
 
 /**
- * LIMITATION: cannot use dynamic keys (e.g. `[exampleRouteConfig.cacheKey]`)
+ * Note: cannot use dynamic keys in the `collections` object
  */
 export const collections = {
-  'example-route': defineCollection({
-    type: 'data',
-    schema: exampleRouteConfig.schema,
-  }),
   'example-channel': defineCollection({
     type: 'data',
     schema: exampleChannelConfig.schema,
+  }),
+  'example-route': defineCollection({
+    type: 'data',
+    schema: exampleRouteConfig.schema,
   }),
   'example-single': defineCollection({
     type: 'data',
